53f98d1a5ad1e17929212bd774564a9d1671e4a1
Emiliano Anichini <emiliano.anichini@gmail.com>
2025-11-04 20:05:32 +0100

    CH1L5

--- main.go
+++ main.go
@@ -12,21 +12,40 @@ func main() {
 	if err != nil {
 		return
 	}
-	defer file.Close()
-	b := make([]byte, 8)
-	line := "read: "
-	for {
-		n, err := file.Read(b)
-		if err == io.EOF {
-			if n > 0 {
-				fmt.Printf("read: %s\n", b[:n])
-			}
-			break
-		} else if err != nil {
-			return
-		}
-		if n > 0 {
-			fmt.Printf("read: %s\n", b[:n])
-		}
+	for s := range getLinesChannel(file) {
+		fmt.Printf("read: %s\n", s)
 	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	b := make([]byte, 8)
+	chnl := make(chan string)
+	go func() {
+		buff := []string{}
+		for {
+			n, err := f.Read(b)
+			res := string(b[:n])
+			if err == io.EOF {
+				if n > 0 {
+					chnl <- strings.Join(buff, "")
+				}
+			}
+			if err != nil {
+				close(chnl)
+				f.Close()
+				break
+			}
+			if n > 0 {
+				s := strings.Split(res, "\n")
+				if len(s) > 0 {
+					buff = append(buff, s[0])
+					for i := 1; i < len(s); i++ {
+						chnl <- strings.Join(buff, "")
+						buff = []string{s[i]}
+					}
+				}
+			}
+		}
+	}()
+	return chnl
 }
